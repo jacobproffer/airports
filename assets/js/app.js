bd979d15fccd809cf8011280f4551cbd9cbefea8
jacobproffer <jacobhardingproffer@gmail.com>
2018-03-14 11:36:21 -0400

    Adjust typography

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,43 +28,43 @@ var style = [
   }
 ];
 var markers = [
-  [35.772, 140.3929, '<h3>Narita International Airport</h3>', 'NRT', 1],
+  [35.772, 140.3929, '<h2>Narita International Airport</h2>', 'NRT', 1],
   [
     32.8998,
     -97.0403,
-    '<h3>Dallas/Forth Worth International Airport</h3>',
+    '<h2>Dallas/Forth Worth International Airport</h2>',
     'DFT',
     2
   ],
   [
     40.6413,
     -73.7781,
-    '<h3>John F. Kennedy International Airport</h3>',
+    '<h2>John F. Kennedy International Airport</h2>',
     'JFK',
     3
   ],
-  [40.7769, -73.874, '<h3>LaGuardia Airport</h3>', 'LGA', 4],
-  [42.3656, -71.0096, '<h3>Logan International Airport</h3>', 'BOS', 5],
-  [42.2162, -83.3554, '<h3>Detroit Metropolitan Airport</h3>', 'DTW', 6],
-  [41.9742, -87.9073, '<h3>OHare International Airport</h3>', 'ORD', 7],
-  [40.1942, -76.7577, '<h3>Harrisburg International Airport</h3>', 'MDT', 8],
+  [40.7769, -73.874, '<h2>LaGuardia Airport</h2>', 'LGA', 4],
+  [42.3656, -71.0096, '<h2>Logan International Airport</h2>', 'BOS', 5],
+  [42.2162, -83.3554, '<h2>Detroit Metropolitan Airport</h2>', 'DTW', 6],
+  [41.9742, -87.9073, '<h2>OHare International Airport</h2>', 'ORD', 7],
+  [40.1942, -76.7577, '<h2>Harrisburg International Airport</h2>', 'MDT', 8],
   [
     44.4834,
     -88.1344,
-    '<h3>Austin Straubel International Airport</h3>',
+    '<h2>Austin Straubel International Airport</h2>',
     'GRB',
     9
   ],
   [
     42.9476,
     -87.8966,
-    '<h3>General Mitchell International Airport</h3>',
+    '<h2>General Mitchell International Airport</h2>',
     'MKE',
     10
   ],
-  [32.4119, -99.68, '<h3>Abilene Regional Airport</h3>', 'ABI', 11],
-  [45.7202, -87.092, '<h3>Delta County Airport', 'ESC', 12],
-  [46.3497, -87.3873, '<h3>Sawyer International Airport', 'MQT', 13]
+  [32.4119, -99.68, '<h2>Abilene Regional Airport</h2>', 'ABI', 11],
+  [45.7202, -87.092, '<h2>Delta County Airport', 'ESC', 12],
+  [46.3497, -87.3873, '<h2>Sawyer International Airport', 'MQT', 13]
 ];
 var focal = { lat: 46.3497, lng: -87.3873 };
 var iconBase = 'http://jacobproffer.com/locations/img/marker.svg';
